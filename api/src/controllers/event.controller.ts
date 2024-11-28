09839a716a214804dc42d7e93f4bc88205b3c416
Logassi <lomonagagaspas@gmail.com>
2024-11-28 21:24:37 +0700

    Feat : Creating connection to DB, and able to Create Event

--- api/src/controllers/event.controller.ts
+++ api/src/controllers/event.controller.ts
@@ -1,7 +1,31 @@
+import { PrismaClient } from "@prisma/client";
 import { Request, Response, NextFunction } from "express";
+
+const prisma = new PrismaClient();
 
 async function CreateEvent(req: Request, res: Response, next: NextFunction) {
   try {
+    const { name, description, price, date, time, location, availableSeats } =
+      req.body;
+
+    await prisma.$transaction(async (prisma) => {
+      await prisma.event.create({
+        data: {
+          name,
+          description,
+          price,
+          date: new Date("2024-12-12T09:00:00"), // Passing as Date object
+          time,
+          location,
+          availableSeats,
+          organizerId: 1,
+        },
+      });
+    });
+
+    console.log("Event Created");
+
+    res.status(201).json({ message: "Event created successfully" });
   } catch (error) {
     next(error);
   }
